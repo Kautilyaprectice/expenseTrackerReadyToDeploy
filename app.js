2a2c0358b0859c19b985c09627052ae84976d95d
Kautilya Tiwari <160465716+Kautilyaprectice@users.noreply.github.com>
2024-07-23 20:14:40 +0530

    new branch changes

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ app.use('/', forgetPasswordRoutes);
 
 
 app.use((req, res, next) => {
-    console.log('url', req.url)
+    console.log('url', req.url);
     res.sendFile(path.join(__dirname, `public/${req.url}`));
 })
 
